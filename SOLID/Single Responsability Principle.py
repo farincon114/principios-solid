84b27c08de1f4300ef42607083f7aaa8e88f65d4
farincon114 <94638283+farincon114@users.noreply.github.com>
2021-11-23 17:56:04 -0500

    Update Single Responsability Principle.py

--- SOLID/Single Responsability Principle.py
+++ SOLID/Single Responsability Principle.py
@@ -1,29 +1,43 @@
 # tienda de ropa online
 
-# 1. Principio de responsabilidad única
-
+# 1. Principio de responsabilidad única CORRECTO
 
 class Cliente:
 
-    def __initial__(self,nombre: str, apellido: str):               #se estan solicitando los datos del cliente
+    def __initial__(self,nombre: str, apellido: str):               #se crea una clase para solicitar los datos del cliente 
         self.nombre = nombre
         self.apellido = apellido
 
 
 class metodos_pago:
 
-    def tarjetas_credito(self,numero:int, fecha:int/int, cvc:int):                                 #se esta asignando los metodos de pago del cliente
+    def tarjetas_credito(self,numero:int, fecha:int/int, cvc:int):                 #se crea otra clase para asignar los metodos de pago del cliente
             self.numero = numero
             self.fecha = fecha
             self.cvc = cvc
             
     def tarjetas_debito(self):
             pass    
+        
+        
+        
+
+# 1. Principio de responsabilidad única INCORRECTO
 
 
+class Cliente:
 
+    def __initial__(self,nombre: str, apellido: str):               #se esta asignando una responsabilidad de solicitar los datos del cliente
+        self.nombre = nombre
+        self.apellido = apellido
 
-
+    def tarjetas_credito(self,numero:int, fecha:int/int, cvc:int):      #se esta asignando otra responsabilidad en la misma clase asignar los metodos de pago del cliente
+            self.numero = numer
+            self.fecha = fecha
+            self.cvc = cvc
+            
+    def tarjetas_debito(self):
+            pass   
 
   
     
